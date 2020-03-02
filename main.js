ddaebde382c85999caca3c0d8c641ed1139639f5
joaosilvalopes <jpgdsl@gmail.com>
2020-03-02 12:37:37 +0000

    feat: Add chasing algorithm and state machine architecture

--- main.js
+++ main.js
@@ -13,44 +13,80 @@ const [host, port] = process.argv.slice(2);
 
 const bot = mineflayer.createBot({
 	host,
-	username: process.env.MC_USERNAME,
-	password: process.env.MC_PASSWORD,
+	username: 'dawd', //process.env.MC_USERNAME,
+	// password: process.env.MC_PASSWORD,
 	port,
 	version: '1.8.9'
 });
 
-navigatePlugin(bot);
-
-const state = {
-	healing: false,
-	enemy: null
-};
+let state = 'waiting';
+let enemy;
+let hotbar;
 
 const attack = throttle(() => {
-	bot.attack(state.enemy);
+	bot.attack(enemy);
 	bot.swingArm();
-}, 250);
+}, 300);
 
 const add = (position, { x = 0, y = 0, z = 0 }) =>
 	vec3(position).add({ x, y, z });
 
-bot.on('entityMoved', entity => {
-	if (entity.type === 'player') {
-		state.enemy = entity;
+const bestPot = () =>
+	hotbar.reduce(
+		(bestPot, item, index) => {
+			if (item && item.type === mcData.itemsByName['potion'].id) {
+				const potion = potions[item.metadata];
 
-		if (state.healing) {
-			return;
-		}
+				if (!potion) {
+					return bestPot;
+				}
 
-		bot.lookAt(add(state.enemy.position, { y: state.enemy.height }));
-		bot.setControlState(
-			'forward',
-			bot.entity.position.distanceTo(state.enemy.position) >= 2.7
-		);
+				const willWasteHealing = potion.healing + bot.health > 20;
 
-		if (bot.entity.position.distanceTo(state.enemy.position) < 2.7) {
-			const hotbar = bot.inventory.slots.slice(-9);
+				if (
+					potion.splash &&
+					potion.healing > bestPot.healing &&
+					!willWasteHealing
+				) {
+					return { healing: potion.healing, index };
+				}
+			}
 
+			return bestPot;
+		},
+		{ healing: -Infinity, index: -1 }
+	);
+
+const shouldHeal = () => bestPot().index !== -1;
+
+const lookAt = (...args) => {
+	return new Promise(resolve => {
+		bot.lookAt(...args, resolve);
+	});
+};
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const run = async () => {
+	hotbar = bot.inventory.slots.slice(-9);
+
+	console.log(state + '');
+	// react to state
+	switch (state) {
+		case 'waiting':
+			break;
+		case 'healing':
+			const pot = bestPot();
+			if (pot.index !== -1) {
+				await lookAt(bot.entity.position, undefined);
+				bot.setQuickBarSlot(pot.index);
+				bot.activateItem();
+			}
+			break;
+		case 'attacking':
+			bot.lookAt(add(enemy.position, { y: enemy.height }));
 			const bestWeapon = hotbar.reduce(
 				(bestWeapon, item, index) => {
 					if (!item) {
@@ -72,50 +108,69 @@ bot.on('entityMoved', entity => {
 				bot.setQuickBarSlot(bestWeapon.index);
 			}
 
+			bot.setControlState('sprint', false);
+			bot.setControlState('jump', false);
+			bot.setControlState(
+				'forward',
+				bot.entity.position.distanceTo(enemy.position) > 1.5
+			);
+
 			attack();
-		}
+			break;
+		case 'chasing':
+			bot.lookAt(add(enemy.position, { y: enemy.height }));
+			bot.setControlState('sprint', true);
+			bot.setControlState('jump', true);
+			bot.setControlState('forward', true);
+			break;
+		default:
+			break;
+	}
+
+	// update state
+	switch (state) {
+		case 'waiting':
+			if (enemy) {
+				state = 'chasing';
+			}
+			break;
+		case 'chasing':
+			if (bot.entity.position.distanceTo(enemy.position) < 2.7) {
+				state = 'attacking';
+			}
+			if (shouldHeal()) {
+				state = 'healing';
+			}
+			break;
+		case 'attacking':
+			if (bot.entity.position.distanceTo(enemy.position) > 2.7) {
+				state = 'chasing';
+			}
+			if (shouldHeal()) {
+				state = 'healing';
+			}
+			break;
+		case 'healing':
+			state =
+				bot.entity.position.distanceTo(enemy.position) < 2.7
+					? 'attacking'
+					: 'chasing';
+			break;
+	}
+
+	setTimeout(run, 200);
+};
+
+run();
+
+bot.on('entityMoved', entity => {
+	if (entity.type === 'player') {
+		enemy = entity;
 	}
 });
 
 bot.on('health', () => {
-	if (bot.health < 20) {
-		const hotbar = bot.inventory.slots.slice(-9);
-		const bestPot = hotbar.reduce(
-			(bestPot, item, index) => {
-				if (item && item.type === mcData.itemsByName['potion'].id) {
-					const potion = potions[item.metadata];
-
-					if (!potion) {
-						return bestPot;
-					}
-
-					const willWasteHealing = potion.healing + bot.health > 20;
-
-					if (
-						potion.splash &&
-						potion.healing > bestPot.healing &&
-						!willWasteHealing
-					) {
-						return { healing: potion.healing, index };
-					}
-				}
-
-				return bestPot;
-			},
-			{ healing: -Infinity, index: -1 }
-		);
-
-		const hasPot = bestPot.index !== -1;
-
-		if (hasPot) {
-			state.healing = true;
-			bot.lookAt(bot.entity.position, undefined, () => {
-				bot.setQuickBarSlot(bestPot.index);
-				bot.activateItem();
-				setTimeout(() => (state.healing = false), 100);
-			});
-		}
-	}
+	bot.chat(`bot hp: ${bot.health}`);
 });
 
 bot.on('error', err => console.log(err));
