3d14f803afe8d29834f8c862d1d3d7930c904f7a
1150450 <email@example.com>
2020-03-19 18:22:15 +0000

    feat: Count sharpness in best weapon algorithm

--- states/Attacking.ts
+++ states/Attacking.ts
@@ -3,6 +3,10 @@ import State from './State';
 import { plus } from '../utils/vec3';
 import weapons from '../weapons';
 import StateId from './StateId';
+import mcData from '../mcData';
+
+const SHARPNESS_ID = mcData.enchantmentsByName["sharpness"].id;
+const SHARPNESS_DMG_MOD = 1.25;
 
 const attack = throttle((bot, enemy) => {
 	bot.attack(enemy);
@@ -28,9 +32,11 @@ class Attacking extends State {
 					return bestWeapon;
 				}
 
-				if (item.name in weapons) {
-					const damage = weapons[item.name];
+				const sharpnessEnchantment = item.nbt?.value.ench.value.value.find((enchantment) => enchantment.id.value === SHARPNESS_ID);
+				const sharpnessDamage = sharpnessEnchantment ? sharpnessEnchantment.lvl.value * SHARPNESS_DMG_MOD : 0;
 
+				if (item.name in weapons) {
+					const damage = weapons[item.name] + sharpnessDamage;
 					return damage > bestWeapon.damage ? { index, damage } : bestWeapon;
 				}
 
