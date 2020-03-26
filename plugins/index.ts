6f68fc5f082209e7cf43bd2c7dc29a73cebd7427
1150450 <email@example.com>
2020-03-26 17:11:17 +0000

    feat:Towering state added

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,4 +1,8 @@
 import util from 'util';
+import armorManager from 'mineflayer-armor-manager';
+import mineflayer from 'mineflayer';
+
+const navigatePlugin = require('mineflayer-navigate')(mineflayer);
 
 const FIRST_HOTBAR_SLOT = 36;
 
@@ -10,19 +14,26 @@ const promisifiedMethods = [
 	'consume'
 ];
 
+const externalPlugins = [
+	(bot) => armorManager(bot, { version: '1.8.9' }),
+	navigatePlugin
+];
+
 const plug = bot => {
 	for (const method of promisifiedMethods) {
 		bot[method] = util.promisify(bot[method]);
 	}
 
-	bot.fromSlotToHand = async (slot: number) => {
+	bot.fromSlotToHand = async (slot: number, handSlot = 0) => {
 		if (slot < FIRST_HOTBAR_SLOT) {
-			await bot.moveSlotItem(slot, FIRST_HOTBAR_SLOT);
-			bot.setQuickBarSlot(0);
+			await bot.moveSlotItem(slot, FIRST_HOTBAR_SLOT + handSlot);
+			bot.setQuickBarSlot(handSlot);
 		} else {
 			bot.setQuickBarSlot(slot - FIRST_HOTBAR_SLOT);
 		}
 	};
+
+	externalPlugins.forEach(plugin => plugin(bot));
 };
 
 export default plug;
---
+++ states/Towering.ts
@@ -0,0 +1,56 @@
+import { Vec3 } from 'vec3';
+import { sleep } from '../utils/async';
+
+import State from './State';
+import StateId from './StateId';
+import mcData from '../mcData';
+
+class Towering extends State {
+
+    getBlockSlotIndex() {
+        const reverseIndex = this.bot.inventory.slots.slice().reverse().findIndex((item) => item && mcData.blocksByName[item.name]);
+        return reverseIndex === -1 ? -1 : 45 - reverseIndex - 1;
+    }
+
+    async moveBlocksToHand() {
+        const slotIndex = this.getBlockSlotIndex();
+        if (slotIndex !== -1) {
+            await this.bot.fromSlotToHand(slotIndex, 8);
+            return true;
+        }
+        return false;
+    }
+
+    async execute() {
+        const { bot } = this;
+
+        bot.lookAt(bot.entity.position);
+        bot.setControlState('jump', true);
+
+        let hasBlockInHand = await this.moveBlocksToHand();
+        while (hasBlockInHand) {
+            try {
+                const block = bot.blockInSight();
+                await bot.placeBlock(block, new Vec3(0, 1, 0));
+                break;
+            } catch (e) {
+                hasBlockInHand = await this.moveBlocksToHand();
+            }
+        }
+    }
+
+    transitionImpl() {
+        /*const { bot, metadata } = this;
+    
+        return (
+            bot.entity.position.distanceTo({ ...metadata.enemy.position, y: bot.entity.position.y }) < 4 &&
+            metadata.enemy.position.y - bot.entity.position.y > 2.7 &&
+            this.getBlockSlotIndex() !== -1
+        ) ? StateId.Towering : StateId.Chasing;*/
+
+        return StateId.Towering;
+    }
+
+}
+
+export default Towering;
